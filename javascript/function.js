0675572382b7bdb073caad1d6e41034b59ea05d5
Tez, Gardner <tezgardner1212@gmail.com>
2019-06-15 15:30:42 -0500

    added event lsiteners for the functions

---
+++ javascript/function.js
@@ -0,0 +1,10 @@
+
+$("#upcomingReleasesBtn").on("click", function(){
+    console.log("Releases Click")
+});
+$("#devPicksBtn").on("click", function(){
+    console.log("Dev Picks Click")
+});
+$("#trendingBtn").on("click", function(){
+    console.log("Trending Click")
+});
