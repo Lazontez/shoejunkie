219c6912f084108763062b06d7214bdf8f6b0405
Tez, Gardner <tezgardner1212@gmail.com>
2019-10-12 21:46:17 -0500

    updated file structure

--- javascript/function.js
+++ javascript/function.js
@@ -1,11 +1,17 @@
 
-$("#upcomingReleasesBtn").on("click", function(){
+$("#upcomingReleasesBtn").on("click", function () {
     $("#frontPage").hide()
     $("#upcomimgReleasesSector").show()
 });
-$("#devPicksBtn").on("click", function(){
-    console.log("Dev Picks Click")
+$("#devPicksBtn").on("click", function () {
+    $("#frontPage").hide()
+    $("#devPicksSector").show()
 });
-$("#trendingBtn").on("click", function(){
-    console.log("Trending Click")
+$("#trendingBtn").on("click", function () {
+    $("#frontPage").hide()
+    $("#trendingShoesSector").show()
+});
+$("#blogBtn").on("click", function () {
+    $("#frontPage").hide()
+    $("#blogSector").show()
 });
