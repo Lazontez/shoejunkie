42a578a2915f92a87eab6a102cecc14273f8a75b
Tez, Gardner <tezgardner1212@gmail.com>
2019-10-20 01:03:30 -0500

    new new

--- client/src/pages/upcomingShoesPage.js
+++ client/src/pages/upcomingShoesPage.js
@@ -10,7 +10,7 @@ class UpcomingShoesPage extends React.Component{
         return(
             <span>
                 <NavBar />
-                
+                <UpcomingShoesHeader />
                 <ShoeContent />
                 <Footer />
 
